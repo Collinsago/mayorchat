0fd9b1f38ad7945c2c8ade6ec03aa6ffab768359
Collins Agofure <71197307+Collinsago@users.noreply.github.com>
2022-06-15 03:09:56 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "Collinsago@@@";
-  $dbname = "chatapp";
+  $hostname = "eu-cdbr-west-02.cleardb.net";
+  $username = "b8bfd6c0820f13";
+  $password = "76f6ae48";
+  $dbname = "heroku_3ad9a7572288722";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
